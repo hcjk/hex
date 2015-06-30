dd5f672e7a945c9b752bfc937afb7ce6edb84741
Colter Giem <coltergiem@gmail.com>
2015-06-30 14:02:03 -0600

    Made colors responsive (rollback if needed)

--- js/index.js
+++ js/index.js
@@ -8,7 +8,14 @@ app.controller('HexCtrl', ['$scope', function ($scope) {
     $scope.$watch(function () {
         return $scope.hex;
     }, function (newVal, oldVal) {
-        if (newVal === 'aaa' || newVal === 'aaaaaa' || newVal === 'bbb' || newVal === 'bbbbbb' || newVal === 'ccc' || newVal === 'cccccc' || newVal === '111' || newVal === '111111' || newVal === '222' || newVal === '222222' || newVal === '333' || newVal === '333333' || newVal === '444' || newVal === '444444'|| newVal === '555' || newVal === '555555' || newVal === '666' || newVal === '666666' || newVal === '777' || newVal === '777777' || newVal === '888' || newVal === '888888' || newVal === '999' || newVal === '999999' || newVal === '000' || newVal === '000000') {
+        var rgb = parseInt(newVal, 16);
+        var r = (rgb >> 16) & 0xff;  // extract red
+        var g = (rgb >>  8) & 0xff;  // extract green
+        var b = (rgb >>  0) & 0xff;  // extract blue
+
+        var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+
+        if (luma < 40 || newVal === 'aaa' || newVal === 'bbb'|| newVal === 'ccc'|| newVal === '111'|| newVal === '222'|| newVal === '333' || newVal === '444' || newVal === '555' || newVal === '666' || newVal === '777' || newVal === '888' || newVal === '999'  || newVal === '000') {
             $scope.newHeaderColor = '#fff';
             $scope.newMainColor = '#ecf0f1';
         } else {
