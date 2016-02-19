f4eb6091ce09003a05f4bf6395754bdca7fbcfe1
Nick DiGiulio <ekyl@users.noreply.github.com>
2016-02-19 15:48:42 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,6 +6,9 @@ app.controller('HexCtrl', ['$scope', function ($scope) {
         console.log('these are bad colors');
     }
     $scope.$watch(function () {
+        if (!(hex.substring(0,1) == '#')) {
+            hex = '#'+hex;
+        }
         return $scope.hex;
     }, function (newVal, oldVal) {
         if (newVal === 'aaa' || newVal === 'aaaaaa' || newVal === 'bbb' || newVal === 'bbbbbb' || newVal === 'ccc' || newVal === 'cccccc' || newVal === '111' || newVal === '111111' || newVal === '222' || newVal === '222222' || newVal === '333' || newVal === '333333' || newVal === '444' || newVal === '444444'|| newVal === '555' || newVal === '555555' || newVal === '666' || newVal === '666666' || newVal === '777' || newVal === '777777' || newVal === '888' || newVal === '888888' || newVal === '999' || newVal === '999999' || newVal === '000' || newVal === '000000') {
